62a2740086ba0e17abe8ed9698b1d8be5dbc3167
Yasser Mohamed Eltahtawy <Yasser.m.eltahtawy@gmail.com>
2025-01-10 19:38:10 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// var allproducts = document.querySelectorAll(".product li")
+
+// var div = document.querySelector("#basket")
+
+//  var btn = document.querySelector("#btn")
+//  var totalprice = 0
+//  var div1 = document.querySelector("#mony")
+
+//  allproducts.forEach(function(item){
+//     item.onclick = function code1(){
+//         div.innerHTML += item.textContent + item.getAttribute("price") +" "
+//         if (div.innerHTML !=" "|| totalprice>0 ){
+//             totalprice += +(item.getAttribute("price"))
+//         }
+//     }
+
+
+
+//  })
+
+//  btn.onclick= function (){
+//      div1.innerHTML += totalprice +" "
+//  }
+
+var x=100
+function code1(){
+    if (false){
+        var x=150
+    }
+    console.log(x)
+}
+ code1()
